b72d5191e271c669c4075e77af1d2fd41ed7f60e
23pallavi <pallavi.y92@gmail.com>
2020-07-28 02:13:29 +0000

    test

---
+++ Java/SortStack.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class SortStack{
+
+    public static void printl()
+    {
+        System.out.println("Hello");
+
+    }
+
+    public static void main(String[] args) {
+        printl();
+        
+    }
+
+
+
+}
