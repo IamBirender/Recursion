5d64cc1d1af1ca35b239584232eb09ff051ee4c4
IamBirender <birender1332.11@gmail.com>
2020-07-28 01:34:43 +0000

    lecture 7 sort a stack

--- sort_stack.py
+++ sort_stack.py
@@ -10,17 +10,20 @@ class sort_stack_with_recursion(object):
     def __recursion(self, arr):
         if len(arr) == 1:
             return arr
-
-        self.__recursion(arr[:-1])
+        temp = arr[-1]
+        arr = self.__recursion(arr[:-1])
+        arr.append(temp)
         res =  self.__insert(arr[:-1], arr[-1])
         return res
-
+    
     def __insert(self, arr, num):
-        if len(arr) ==0 or arr[-1] > num:
-            return num
-
-        self.__insert(arr[:-1], num)
-
+        if len(arr) == 0 or num > arr[-1]:
+            arr.append(num)
+            return arr
+        temp = arr[-1]
+        arr = self.__insert(arr[:-1], num)
+        arr.append(temp)
+        return arr
 
 arr = [4,5,2,3,1,8,9]
 sort_stack = sort_stack_with_recursion(arr)
