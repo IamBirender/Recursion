81ae25e5accf637711930a807da34837d35def8f
IamBirender <birender1332.11@gmail.com>
2020-07-31 21:11:10 +0000

    permutations with case change video 13

---
+++ perm_with_case_change.py
@@ -0,0 +1,20 @@
+class perm_with_case_change(object):
+    def __init__(self, string):
+        self.string = string
+        self.result = list()
+
+    def _get_permutations(self):
+        self.__hypothesis(self.string, "")
+
+    def __hypothesis(self, input, output):
+        if len(input) == 0:
+            self.result.append(output)
+            return 
+
+        self.__hypothesis(input[1:], output+input[0] )
+        self.__hypothesis(input[1:], output+input[0].upper())
+    
+string = "abcd"
+obj = perm_with_case_change(string)
+obj._get_permutations()
+print(obj.result)
