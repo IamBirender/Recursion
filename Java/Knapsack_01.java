67cf8473dfe648fffee738376115847fee99020a
23pallavi <pallavi.y92@gmail.com>
2020-08-06 01:08:25 +0000

    knapsack java code

---
+++ Java/Knapsack_01.java
@@ -0,0 +1,36 @@
+package Java;
+public class Knapsack_01
+{
+    static int res = 0;
+    public static int knapsack(int[] weight, int[] value, int capacity, int N)
+    {
+        if (N == 0 || capacity == 0)
+        {
+            return 0;
+        }
+
+        if(weight[N-1] > capacity)
+        {
+            return knapsack(weight, value, capacity, N-1);
+        }
+
+        else
+        {
+            return Math.max(value[N-1]+knapsack(weight, value, capacity - weight[N-1], N-1), knapsack(weight, value, capacity, N-1));
+            
+        }
+       
+       
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        int[] weight = {1,3,4,5};
+        int[] value = {1,4,5,7};
+        int capacity = 7;
+        int N = weight.length;
+        System.out.println(knapsack(weight, value, capacity, N));
+    }
+}
